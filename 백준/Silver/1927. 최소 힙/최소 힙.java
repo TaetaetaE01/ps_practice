ed8267f349c569f75dd72c813376d9fff11bd055
Taehyeon Ko <68328998+TaetaetaE01@users.noreply.github.com>
2026-03-11 16:25:16 +0900

    [Silver II] Title: 최소 힙, Time: 244 ms, Memory: 26128 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -7,18 +7,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
             int x = Integer.parseInt(br.readLine());
-            
+
             if (x == 0 && minHeap.isEmpty()) {
                 minHeap.add(x);
-                System.out.println(minHeap.poll());
+                sb.append(minHeap.poll()).append('\n');
             } else if (x == 0) {
-                System.out.println(minHeap.poll());
+                sb.append(minHeap.poll()).append('\n');
             } else {
                 minHeap.add(x);
             }
         }
+        System.out.println(sb);
     }
 }
