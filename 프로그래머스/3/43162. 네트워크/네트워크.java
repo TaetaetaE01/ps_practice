b5a3c2deaca4b5bb14a427725c1cf070a4c520c2
Taehyeon Ko <68328998+TaetaetaE01@users.noreply.github.com>
2026-04-04 17:36:48 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,29 @@
 class Solution {
     
-    static boolean[] visit;
+    boolean[] visit;
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
         visit = new boolean[n];
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 0 ; i < n ; i ++){
             if(visit[i] == false){
+                // dfs 탐색에서 중복 탐색이 안되도록 하기 위해
                 answer++;
-                dfs(i, computers, n);
-            }    
+                dfs(i, n, computers);
+            }
         }
-        
         return answer;
     }
     
-    public void dfs(int i, int[][] computers , int n ){
+    public void dfs(int i , int n , int[][] computers){
         visit[i] = true;
         
-        for(int j = 0 ; j < n ; j++){
-           if(visit[j] == false && computers[i][j] == 1){
-                dfs(j, computers, n);
-           } 
+        for(int j = 0; j < n ; j++){
+            if(!visit[j] && computers[i][j] == 1){
+                visit[j] = true;
+                dfs(j, n, computers);
+            }
         }
     }
 }
