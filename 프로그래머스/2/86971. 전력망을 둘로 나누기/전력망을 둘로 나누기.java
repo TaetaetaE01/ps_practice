0ca74521da4f23d677bc9a1fed278c4d437d31e0
Taehyeon Ko <68328998+TaetaetaE01@users.noreply.github.com>
2026-04-24 18:09:10 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.47 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,35 +1,45 @@
 class Solution {
     
-    static int[][] graph;
+    int[][] graph;
     
     public int solution(int n, int[][] wires) {
-        int answer = n;
-        graph = new int[n+1][n+1];
+        // wires : 연결된 전선의 정보, wires.length는 전선의 개수
+        // graph : 송전탑(node라 볼 수 있음)
+        // n : 송전탑 개수
+        // 전선 정보를 바탕으로 송전탑을 양방향으로 연결
+        // 전선을 하나씩 끊어서 양 덩어리의 차(-)의 최솟값을 구해야함
+        // 모든 전선을 끝까지 다 끊어보고 비교해야되서 DFS, for문으로 모두 탐색해야됨
         
+        int answer = n;
+        graph = new int[n+1][n+1]; // 송전탑 번호는 1번부터 시작이라
+        
+        // 송전탑 세팅
         for(int i = 0 ; i < wires.length ; i ++){
             int v1 = wires[i][0];
             int v2 = wires[i][1];
             
-            // 각 송전탑을 양방향으로 연결
+            // 각 송전탑을 양방향으로 연결 : 1
             graph[v1][v2] = 1;
             graph[v2][v1] = 1;
         }
         
-        for(int i = 0 ; i < wires.length ; i ++){
+        // 각 전선마다 다 끊어보고 찾기
+        for(int i = 0 ; i < wires.length ; i++){
             int v1 = wires[i][0];
             int v2 = wires[i][1];
             
-            // 각 송전탑을 양방향 하나씩 끊기
+            // 송전탑 전선을 하나씩 끊기 : 0
             graph[v1][v2] = 0;
             graph[v2][v1] = 0;
             
-            boolean visit[] = new boolean[n+1];
+            // 낮은번호의 송전탑에 연결된 개수 dfs로 찾기
+            boolean[] visited = new boolean[n+1];
             
-            int cnt = dfs(v1, visit, n);
-            int diff = Math.abs(cnt -(n - cnt));
-            answer = Math.min(diff, answer);
+            int count = dfs(v1, visited, n);
+            int temp = Math.abs(count -(n-count));
+            answer = Math.min(answer, temp);
             
-            // 다시 연결시켜 줘야 함 다음 송전탑기준으로 계산하기 위해서
+            // 송전탑 다시 연결시켜서 초기화 시켜줘야 함
             graph[v1][v2] = 1;
             graph[v2][v1] = 1;
         }
@@ -37,13 +47,16 @@ class Solution {
         return answer;
     }
     
-    public int dfs(int v1, boolean[] visit, int n){
-        visit[v1] = true;
+    public int dfs(int v1, boolean[] visited, int n){
+        // 자기 자신 송전탑 개수는 포함
         int count = 1;
+        visited[v1] = true;
         
-        for(int i = 1 ; i <= n ; i ++){
-            if(!visit[i] && graph[v1][i] == 1){
-                count += dfs(i, visit, n);
+        // 송전탑 기준으로 돌 탐색할 것이기 때문
+        for(int i = 1; i <= n ; i ++){
+            // 끊어진 송전탑 기준으로 주위 송전탑 개수 탐색
+            if(graph[v1][i] == 1 && !visited[i]){
+                count += dfs(i, visited, n);
             }
         }
         return count;
